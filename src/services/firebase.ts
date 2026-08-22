a6563291e04384ba0cfc33211c336b7914928f97
Pearlin <dadhaniapsd@gmail.com>
2026-08-22 13:57:39 +0530

    language and notification complete

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -14,7 +14,9 @@ const firebaseConfig = {
 };
 
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
